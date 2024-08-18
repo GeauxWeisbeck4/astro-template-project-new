a037ef493d47df6995c3f63838cf797c85597a3b
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2024-08-18 01:12:17 -0400

    Commit latest changes to index page

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+    site_name: "Astro Template Project New",
+    title: "Geaux Astro | Template by Geaux Code",
+    description: "A starter template created by Geaux Code",
+    author: "Andrew Weisbeck",
+    locale_region: "us-en",
+    locale: "en"
+};
